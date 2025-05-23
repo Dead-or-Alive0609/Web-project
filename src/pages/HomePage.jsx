62d308d49dacef135af80ba3273518fdeb727c63
Dead-or-Alive0609 <heuiwon716@gmail.com>
2025-05-24 01:50:50 +0900

    홈페이지 이미지 첨부 api 사용해서 하는 걸로 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,90 +1,109 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/HomePage.css';
 
-const bookList = [
-  { title: "세이노의 가르침", authors: ["세이노"] },
-  { title: "불변의 법칙", authors: ["모건 하우절"] },
-  { title: "하얼빈", authors: ["김훈"] },
-  { title: "나미야 잡화점의 기적", authors: ["히가시노 게이고"] },
-  { title: "문과 남자의 과학 공부", authors: ["이선영"] },
-  { title: "진짜 공부", authors: ["신영준", "고영성"] },
-  { title: "작별하지 않는다", authors: ["한강"] },
-  { title: "메리골드 마음 세탁소", authors: ["윤정은"] },
-  { title: "죽고 싶지만 떡볶이는 먹고 싶어", authors: ["백세희"] },
-  { title: "내가 틀릴 수도 있습니다", authors: ["비욘 나티코 린데블라드"] },
-  { title: "도둑맞은 집중력", authors: ["요한 하리"] },
-  { title: "마흔에 읽는 쇼펜하우어", authors: ["강용수"] },
-];
-
 function HomePage() {
   const navigate = useNavigate();
-  const [bestNovels, setBestNovels] = useState([]);
 
-  useEffect(() => {
-    const fetchThumbnails = async () => {
-      const results = await Promise.all(
-        bookList.map(async (book) => {
-          try {
-            const res = await fetch(
-              `https://dapi.kakao.com/v3/search/book?target=title&query=${encodeURIComponent(book.title)}`,
-              {
-                headers: {
-                  Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_API_KEY}`,
-                },
-              }
-            );
-      const data = await res.json();
-      const thumbnail =
-        data.documents && data.documents.length > 0
-          ? data.documents[0].thumbnail
-          : null;
+  const bestNovels = [
+    {
+      isbn: "9788998441012",
+      title: "모순순",
+      authors: ["양귀자"],
+      thumbnail: "https://image.yes24.com/goods/8759796/XL",
+    },
+    {
+      isbn: "9788937473401",
+      title: "급류",
+      authors: ["정대건"],
+      thumbnail: "https://image.yes24.com/goods/116586303/XL",
+    },
+    {
+      isbn: "9791162203620",
+      title: "파과",
+      authors: ["구병모"],
+      thumbnail: "https://image.yes24.com/goods/125761518/XL",
+    },
+    {
+      isbn: "9788936434120",
+      title: "소년이 온다",
+      authors: ["한강"],
+      thumbnail: "https://image.yes24.com/goods/13137546/XL",
+    },
+    {
+      isbn: "9791168127012",
+      title: "파쇄",
+      authors: ["구병모"],
+      thumbnail: "https://image.yes24.com/goods/117828174/XL",
+    },
+    {
+      isbn: "9788954681155",
+      title: "홍학의 자리",
+      authors: ["정해연"],
+      thumbnail: "https://image.yes24.com/goods/102791425/XL",
+    },
+    {
+      isbn: "9791197221989",
+      title: "첫 여름, 완주",
+      authors: ["김금희"],
+      thumbnail: "https://image.yes24.com/goods/145567095/XL",
+    },
+    {
+      isbn: "9788936439743",
+      title: "혼모노",
+      authors: ["성해나"],
+      thumbnail: "https://image.yes24.com/goods/143911524/XL",
+    },
+    {
+      isbn: "9791168342880",
+      title: "고래눈이 내리다",
+      authors: ["김보영"],
+      thumbnail: "https://image.yes24.com/goods/146651623/XL",
+    },
+    {
+      isbn: "9788936437978",
+      title: "대도시의 사랑법 (15만부 기념 특별 한정판)",
+      authors: ["박상영"],
+      thumbnail: "https://image.yes24.com/goods/74971092/XL",
+    },
+    {
+      isbn: "9791141087845",
+      title: "마침내 멸망하는 여름",
+      authors: ["정"],
+      thumbnail: "https://image.yes24.com/goods/127167526/XL",
+    },
+    {
+      isbn: "9791167372864",
+      title: "구의 증명",
+      authors: ["최진영"],
+      thumbnail: "https://image.yes24.com/goods/118578901/XL",
+    },
+  ];
 
-      return {
-        ...book,
-        isbn: data.documents[0]?.isbn?.split(" ")[0] || book.title,
-        thumbnail,
-      };
-    } catch (err) {
-      console.error(`Error fetching ${book.title}:`, err);
-      return { ...book, thumbnail: null };
-    }
-  })
-      );
-  setBestNovels(results);
-};
+  const handleClick = (isbn) => {
+    navigate(`/detail/${isbn}`);
+  };
 
-fetchThumbnails();
-  }, []);
-
-const handleClick = (isbn) => {
-  navigate(`/detail/${isbn}`);
-};
-
-return (
-  <div className="home-container">
-    <h2>📚 2025년 소설 베스트셀러 TOP 12</h2>
-    <div className="book-grid">
-      {bestNovels.map((book, idx) => (
-        <div
-          key={book.isbn}
-          className="book-item"
-          onClick={() => handleClick(book.isbn)}
-        >
-          {book.thumbnail ? (
+  return (
+    <div className="home-container">
+      <h2>📚 2025년 소설 베스트셀러 TOP 12</h2>
+      <div className="book-grid">
+        {bestNovels.map((book, idx) => (
+          <div
+            key={book.isbn}
+            className="book-item"
+            onClick={() => handleClick(book.isbn)}
+          >
             <img src={book.thumbnail} alt={book.title} />
-          ) : (
-            <div className="no-img">이미지 없음</div>
-          )}
-          <div className="book-title">
-            {idx + 1}위. {book.title}
+            <div className="book-title">
+              {idx + 1}위. {book.title}
+            </div>
+            <div className="book-author">{book.authors.join(', ')}</div>
           </div>
-          <div className="book-author">{book.authors.join(', ')}</div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default HomePage;
